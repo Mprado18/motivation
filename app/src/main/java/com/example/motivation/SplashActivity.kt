9ddca2f8361f374f8af84d6823f2992d69c602db
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-06-07 15:52:30 -0300

    created save name function and event click button save

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -1,17 +1,37 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.motivation.databinding.ActivitySplashBinding
+import com.example.motivation.infra.SecurityPreferences
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
+
+    private lateinit var  mSecurityPreferences: SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.buttonSave.setOnClickListener { handleSave() }
+
+        mSecurityPreferences = SecurityPreferences(this)
+    }
+
+    private fun handleSave() {
+        val name = binding.inputText.text
+
+        if (name.isNotEmpty()) {
+            mSecurityPreferences.storeString("name", name.toString())
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "Informe seu nome!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
