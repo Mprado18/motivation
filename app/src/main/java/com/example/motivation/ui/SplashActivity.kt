95e2313c6194d0ab979b2db76a434eb3252d0ef6
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-06-08 11:35:37 -0300

    added new logic and comments

--- app/src/main/java/com/example/motivation/ui/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.motivation.databinding.ActivitySplashBinding
+import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.infra.SecurityPreferences
 
 class SplashActivity : AppCompatActivity() {
@@ -22,14 +23,29 @@ class SplashActivity : AppCompatActivity() {
         binding.buttonSave.setOnClickListener { handleSave() }
 
         mSecurityPreferences = SecurityPreferences(this)
+
+        verifyName()
     }
 
+    //verifica se nome já foi salvo e recupera o mesmo
+    private fun verifyName() {
+        val name = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        if (name != "") {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    //valida se foi inputado nome, caso não exibe toast, caso sim, salva nome e segue para outra tela
     private fun handleSave() {
         val name = binding.inputText.text
 
         if (name.isNotEmpty()) {
-            mSecurityPreferences.storeString("name", name.toString())
+            mSecurityPreferences.storeString(MotivationConstants.KEY.PERSON_NAME, name.toString())
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         } else {
             Toast.makeText(this, "Informe seu nome!", Toast.LENGTH_SHORT).show()
         }
